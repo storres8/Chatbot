fb9a9fcd72c63720ce66fc66ed33b39c9998a639
storres8 <stevetorres7562@gmail.com>
2019-05-31 19:57:41 -0400

    loaded in socket.io into the backend

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+//creates a new sever
+const server = http.createServer(app);
+// passing in the server we created into socket.io to use websockets
+const io = socketio(server);
 
 // allows us to parse the body of an incoming request
 app.use(express.json());
-
 const port = process.env.PORT || 5500;
 
 app.get("/", (req, resp) => {
   resp.send("working");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
