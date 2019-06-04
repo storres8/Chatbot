38416057944a92617496081ab494117c2e4ac757
storres8 <stevetorres7562@gmail.com>
2019-06-04 09:39:53 -0400

    created remove user helper function.

--- utils/users.js
+++ utils/users.js
@@ -38,6 +38,14 @@ const addUser = user => {
   return { user: user };
 };
 
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
 module.exports = {
   addUser
 };
