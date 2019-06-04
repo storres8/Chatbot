d16f81bad65307a186e17391324f9d62113814b8
storres8 <stevetorres7562@gmail.com>
2019-06-03 21:59:22 -0400

    created addUsers helper function.

---
+++ utils/users.js
@@ -0,0 +1,43 @@
+const users = [];
+
+// add users into the users array
+const addUser = user => {
+  // user argument is an object with an id, username, & room.
+
+  // clean the incomng data.
+  const username = user.username.trim().toLowerCase();
+  const room = user.room.trim().toLowerCase();
+  const id = user.id;
+
+  //   validate the data.
+  if (!username || !room) {
+    return {
+      error: "Username and room are required."
+    };
+  }
+
+  //   if there is a room provided, check to see if username is already being used.
+  const existingUser = users.find(user => {
+    return user.room === room && user.username === username;
+  });
+
+  //   if existingUser is true then there is already someone with that username in the room.
+  if (existingUser) {
+    return {
+      error: "That username has already been taken."
+    };
+  }
+
+  //   If no errors then add user to the users array.
+  users.push({
+    id: id,
+    username: username,
+    room: room
+  });
+
+  return { user: user };
+};
+
+module.exports = {
+  addUser
+};
